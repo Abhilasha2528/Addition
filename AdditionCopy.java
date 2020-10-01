ec5831a3e0ff58af32f3473767244d67162a3383
Abhilasha2528 <abhilasha.etikala@gmail.com>
2020-10-01 15:36:39 +0530

    Updated addtn file

---
+++ AdditionCopy.java
@@ -0,0 +1,17 @@
+package com.devops.calculator.addition;
+
+/**
+ *Addition od two numbers!
+ *
+ */
+ 
+ 
+public class Addition {
+	public double addition(double a, double b){
+		return a+b;
+	}
+	public double addition(double a, double b, double c){
+		return a+b+c;
+		//return a+b+c;
+	}
+}	
